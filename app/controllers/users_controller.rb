03044e63027ddbd34502c2bbea0ea2b1aacf39d6
Tatsuro <tatsurow1122@gmail.com>
2020-04-15 23:34:37 +0900

    [Update] 5-8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,27 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id]
-    post_images = @user.post_images.page(params[:page]).reverse_order
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+
   end
 
   def new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '626f99e2a345275e7359da24b42574ac0f5daa8065cd982ced7d500cc884c80f6e20696f9ee86c1559bd9abf82f97345c86d172cacef6411eee4166e13e6f1d0'
